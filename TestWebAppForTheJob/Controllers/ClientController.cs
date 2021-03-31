473a69f7aa4fa026160be07dbc7e7d865712af69
GeromGG <ty.gerom@gmail.com>
2021-03-31 23:47:11 +0300

    ClientController is full of content / ClientController наполнен содержимым

--- TestWebAppForTheJob/Controllers/ClientController.cs
+++ TestWebAppForTheJob/Controllers/ClientController.cs
@@ -3,10 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestWebAppForTheJob.Data.Interfaces;
 
 namespace TestWebAppForTheJob.Controllers
 {
     public class ClientController : Controller
     {
+        private readonly IAllClient _allClient;
+        private readonly IClientFounders _clientFounders;
+
+        public ClientController(IAllClient allClient, IClientFounders clientFounders)
+        {
+            _allClient = allClient;
+            _clientFounders = clientFounders;
+        }
+
+        public ViewResult ListClient()
+        {
+            var client = _allClient.Clients;
+            return View(client);
+        }
     }
 }
