3eda79916bcbc2a8703856957d0aab61ba3b941d
GeromGG <ty.gerom@gmail.com>
2021-03-31 21:46:22 +0300

    mocks created / созданы макеты

---
+++ TestWebAppForTheJob/Data/Mocks/MockClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebAppForTheJob.Data.Interfaces;
+using TestWebAppForTheJob.Data.Models;
+
+namespace TestWebAppForTheJob.Data.Mocks
+{
+    public class MockClient : IAllClient
+    {
+        private readonly IClientFounders _clientFounders = new MockFounder();
+        public IEnumerable<Client> Clients 
+        {
+            get {
+                return new List<Client>
+                {
+                    new Client(id:0, inn:"123456789000", name:"ФондИвест", type: true, founder: _clientFounders.AllFounders.First()),
+                    new Client(id:1, inn:"123456789033", name:"Моэстро", type: false, founder: _clientFounders.AllFounders.Last()),
+                    new Client(id:2, inn:"123456789056", name:"ГолдБест", type: true, founder: _clientFounders.AllFounders.First()),
+                };
+            } 
+        }
+
+        public IEnumerable<Client> GetEntity { get; set; }
+        public IEnumerable<Client> GetIndividualEntrepreneur { get; set; }
+
+        public Client GetObjectClient(int clientID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestWebAppForTheJob/Data/Mocks/MockFounder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebAppForTheJob.Data.Interfaces;
+using TestWebAppForTheJob.Data.Models;
+
+namespace TestWebAppForTheJob.Data.Mocks
+{
+    public class MockFounder : IClientFounders
+    {
+        public IEnumerable<Founder> AllFounders 
+        {
+            get {
+                return new List<Founder>
+                {
+                    new Founder(id:0, inn:"000004400401", fullName:"Александров Александр Александрович"),
+                    new Founder(id:1, inn:"000000000001", fullName:"Иванов Иван Иванович"),
+                    new Founder(id:2, inn:"000000000002", fullName:"Петров Петр Петрович"),
+                    new Founder(id:3, inn:"000000300001", fullName:"Сидоров Сидор Сидорович"),
+                };
+            } 
+        }
+    }
+}
