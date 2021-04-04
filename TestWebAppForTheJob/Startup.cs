e97918113a5a37dce17b31fe084831d88e7ef8d8
GeromGG <ty.gerom@gmail.com>
2021-04-04 21:37:14 +0300

    removed unnecessary in the project / убрано лишнее в проекте

--- TestWebAppForTheJob/Startup.cs
+++ TestWebAppForTheJob/Startup.cs
@@ -12,7 +12,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using TestWebAppForTheJob.Data;
 using TestWebAppForTheJob.Data.Interfaces;
-using TestWebAppForTheJob.Data.Mocks;
 using TestWebAppForTheJob.Data.Repository;
 
 namespace TestWebAppForTheJob
@@ -25,21 +24,17 @@ namespace TestWebAppForTheJob
         {
             _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDBContext>(obtions => obtions.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            //services.AddTransient<IAllClients, MockClient>();
-            //services.AddTransient<IClientFounders, MockFounder>();
             services.AddTransient<IAllClients, ClientRepository>();
             services.AddTransient<IClientFounders, FounderRepository>();
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -57,14 +52,11 @@ namespace TestWebAppForTheJob
                     template: "{controller=Clients}/{action=ListClients}/{id?}");
             });
 
-
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 AppDBContext context = scope.ServiceProvider.GetRequiredService<AppDBContext>();
                 DBObjects.Initial(context);
             }
-
-            
         }
     }
 }
